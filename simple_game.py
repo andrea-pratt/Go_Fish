551596cfaf7d25b94cf26ebd4bb8a903cddc0231
Andrea <apratt1013@gmail.com>
2021-01-18 16:16:19 -0600

    fixed bugs and added game ending

--- simple_game.py
+++ simple_game.py
@@ -1,12 +1,13 @@
-import random 
-import dataclasses
+import random
 import collections
+import sys
 
 cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 deck = []
 
 # There will be two players in this game. The user, and the computer
 class Player:
+
     def __init__(self, name, hand):
         self.name = name
         self.hand = hand
@@ -14,8 +15,6 @@ class Player:
 
     def __str__(self):
         return f'Name: {self.name} Hand: {self.hand} Books: {self.books}'
-
-
 
 # This function creates a shuffled deck with 52 cards
 def create_deck():
@@ -57,6 +56,7 @@ def player1_turn():
     if match_found:
         print('You found a match. You get to go again!!')
         check_for_books(player_1)
+        print(computer)
         player1_turn()
     else:
         print('GO FISH!!')
@@ -91,20 +91,21 @@ def computer_turn():
 
 def request_card(card, requester, donator):
     match_found = False
-    for c in donator.hand:
-        if c == card:
-            requester.hand.append(c)
-            donator.hand.remove(c)
-            match_found = True
+    while card in donator.hand:
+        donator.hand.remove(card)
+        requester.hand.append(card)
+        match_found = True
     return match_found
 
 
 def go_fish(player):
      if len(deck) > 0:
+        print(f'The card that was chosen from the deck is {deck[0]}')
         player.hand.append(deck[0])
         deck.remove(deck[0])
      else:
-         print('The game is over. There are no cards remaining in the deck.')
+         determine_results()
+         sys.exit()
 
 def check_for_books(player):
     print('This method will check to see if a user has any new books.')  
@@ -113,13 +114,23 @@ def check_for_books(player):
     for key in counter:
         if counter[key] == 4:
             player.books += 1
-            for c in player.hand:
-                if c == key:
-                    player.hand.remove(key)
+            while key in player.hand:
+                player.hand.remove(key)
             print(f'{player.name} made a book of {key}\'s')
+            print(player_1)
+            print(computer)
 
 
-
+def determine_results():
+    print('The game is over. There are no more cards in the deck.\n')
+    if computer.books > player_1.books:
+        print('The COMPUTER won...\n')
+    elif computer.books == player_1.books:
+        print('It was a tie...\n')
+    else:
+        print('YOU WON!!!\n')
+    print(f'Your books: {player_1.books}\nComputer books: {computer.books}')
+    
 
 deck = create_deck()
 deal_hand(deck)
