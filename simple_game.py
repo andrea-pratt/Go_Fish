b080544f00b2e7c41d971a8ea25f9c29c0d101b2
Andrea <apratt1013@gmail.com>
2021-01-18 04:24:10 -0600

    added a Player class

---
+++ simple_game.py
@@ -0,0 +1,10 @@
+import random 
+import dataclasses
+
+# There will be two players in this game. The user, and the computer
+class Player:
+    def __init__(self, name, hand):
+        self.name = name
+        self.hand = []
+
+
