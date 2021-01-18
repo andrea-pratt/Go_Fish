bd6dfac25c81fe6c476faf6dbabd93dfa71c2992
Andrea <apratt1013@gmail.com>
2021-01-18 04:39:29 -0600

    Added a create_deck function

--- simple_game.py
+++ simple_game.py
@@ -1,6 +1,8 @@
 import random 
 import dataclasses
 
+cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
 # There will be two players in this game. The user, and the computer
 class Player:
     def __init__(self, name, hand):
@@ -8,3 +10,10 @@ class Player:
         self.hand = []
 
 
+def create_deck():
+    deck = cards*4
+    random.shuffle(deck)
+    print(deck)
+    print(len(deck))
+
+create_deck()
